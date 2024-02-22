8e7b36a383b9d31a3dae9049c70fa3a5df98f5b9
Amélie Gruel <43147713+ameliegruel@users.noreply.github.com>
2024-02-22 15:31:28 +0100

    Improve vid2frames with dataset and fps given as optionnal arguments

--- RGBframes_2_synthetic_events/vid2frames.sh
+++ RGBframes_2_synthetic_events/vid2frames.sh
@@ -1,13 +1,39 @@
-# HERE: change dataset path to where the mp4 videos are stored
-dataset="/home/amelie/Scripts/APROVIS3D_CoastlineDetection/data/2022_i3S_TestBeachDataset/"
+# Bash script to transform a RGB video (originally in MP4 / AVI / FLV) into grayscale frames (later used by vid2e to produce events)
+# Takes as input argument:
+#   1) dataset (mandatory): directory where the RGB video(s) are stored
+#   2) fps (optionnal, default=24): number of frames per second in the input RGB video
+# 05/21 - Amélie Gruel (I3S, Université Côte d'Azur, France)
 
-fps=24  # 24 frames per second
+dataset=$1
+fps=24
+
+print_usage() {
+  printf "Usage: ..."
+}
+
+while getopts 'f:' flag; do
+  case "${flag}" in
+    f) fps="${OPTARG}" ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
 
 for file in $(ls -R $dataset); do
-    if [[ ${file: -4} == ".MP4" ]]; then
-        echo $file;
-        mkdir -p $path"frames_"${file::-4}/;
-        ffmpeg -i $path$file -vf hue=s=0 -r $fps $path"frames_"${file::-4}/frame_%06d.png;
+    filename=`echo $file | sed 's,^.*[^/]*/,,'`
+    if [[ ${filename: -4} == ".mp4" ]] || [[ ${filename: -4} == ".flv" ]] || [[ ${filename: -4} == ".AVI" ]]; then
+
+        START=$(date +%s.%N)
+
+        echo $filename 
+        mkdir -p $dataset"frames_"${filename::-4}/;
+        ffmpeg -i $path$file -vf hue=s=0 -r $fps $dataset"frames_"${filename::-4}/frame_%06d.png;
+
+        END=$(date +%s.%N)
+        DIFF=$(echo "$END - $START" | bc)
+        echo "time" $filename ":" $DIFF
+        echo ""
+
     elif [[ $file =~ ^/.* ]]; then
         path=${file::-1}/;
     fi
